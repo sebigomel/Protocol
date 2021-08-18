4bb8fde5f18989b55c6a80bd914f16855f12a441
sebigomel <67985847+sebigomel@users.noreply.github.com>
2021-08-17 22:35:34 -0300

    first routr

--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const app = express()
+const usersRoutes = require('./routes/users')
+
+const app = express();
+app.use('/users', usersRoutes);
+
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://sebigomel:Mikemike04@cluster-class-app.nw0nx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser : true, useUnifiedTopology : true})
+        .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+        .catch((err) => console.log(err.message))
+
+mongoose.set('useFindAndModify', false)
