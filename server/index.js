9245b9cf0c96b34746d081da50a1682cdd286fdc
Sebastian Gomel <sebigomel@gmail.com>
2021-08-31 16:43:34 -0300

    iarita

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const {connect, set} = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 const passport = require('passport');
+const glob = require('glob')
 const { initialize } = require('passport');
 
 require('dotenv').config();
@@ -16,11 +17,18 @@ app.use(express.urlencoded());
 
 app.use(passport.initialize());
 
+glob.sync("./routes/**/*.js").forEach(function (file) {
+        let modulePath = path.resolve(file)
+        let route = modulePath.split("routes")[1].replace(/\\/g, '/').replace(".js", "").split('/')
+        if ([...route].pop() == "index") route.pop()
+        require(modulePath)(app, route.join('/'));
+});
 const CONNECTION_URL = process.env.DATABASE_URL;
 const PORT = process.env.PORT || 5000;
 
 connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
         .catch((err) => console.log(err.message))
 
 set('useFindAndModify', false)
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
