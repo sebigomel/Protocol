873996437aa2d34a424620f7e2f5bc457dc090c1
Matilde <71106599+Matilde344@users.noreply.github.com>
2021-09-09 08:24:00 -0300

    halpe

--- client/src/Pages/LogIn.jsx
+++ client/src/Pages/LogIn.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
+import './Login.css'
 
 export default function LogIn() {
     return (
         <div className="login">
 
-            <div className="nav">
-                <div className="protocol">
-                    <img src="" alt="" />
-                    <span>protocol</span>
-                </div>
-                <a>m</a>
-            </div>
+                <nav class="navbar navbar-dark bg-light custom-nav">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#">
+                            <img src="/logo512.png" alt="" width="24" height="24" className='nav-icon'/>
+                            <span style={{verticalAlign: 'middle'}}>Protocol</span>
+                        </a>
+                    </div>
+                </nav>
 
             <div className="form">
-                <span>iniciar sesion</span>
-                <span>nombre de usuario</span>
-                <input></input>
+                <h3>Iniciar sesion:</h3>
+                <input className='login-input' type='text' placeholder='Nombre de usuario'></input>
+                <input className='login-input' type='password' placeholder='Contraseña'></input>
+                <div className="form-footer">
+                    <button className='btn btn-primary custom-btn'>Iniciar sesion</button>
+                    <a href="#">Olvide mi contraseña</a>
+                </div>
             </div>
 
-            <div className="footer"></div>
+            <div className="footer">
+                <span>Protocol</span>
+            </div>
         </div>
     )
 }
