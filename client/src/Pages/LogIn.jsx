cf189b45040ee31029bcd21650a610fbccd77cff
Matilde <71106599+Matilde344@users.noreply.github.com>
2021-09-10 11:22:56 -0300

    cambios de material ui

--- client/src/Pages/LogIn.jsx
+++ client/src/Pages/LogIn.jsx
@@ -1,25 +1,47 @@
 import React from 'react'
 import './Login.css'
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 export default function LogIn() {
+    const classes = useStyles();
     return (
         <div className="login">
 
-                <nav class="navbar navbar-dark bg-light custom-nav">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#">
-                            <img src="/logo512.png" alt="" width="24" height="24" className='nav-icon'/>
-                            <span style={{verticalAlign: 'middle'}}>Protocol</span>
-                        </a>
-                    </div>
-                </nav>
+            <AppBar position="static">
+                <Toolbar>
+                    <img src="/logo512.png" alt="" width="24" height="24" className='nav-icon' />
+                    <Typography variant="h6" className={classes.title}>
+                        Protocol
+          </Typography>23
+          {/* kdnfgkjfhgihgirht4 */}
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
 
             <div className="form">
                 <h3>Iniciar sesion:</h3>
                 <input className='login-input' type='text' placeholder='Nombre de usuario'></input>
                 <input className='login-input' type='password' placeholder='Contraseña'></input>
                 <div className="form-footer">
-                    <button className='btn btn-primary custom-btn'>Iniciar sesion</button>
+                    <Button className='custom-btn' variant='contained' color='primary'>Iniciar sesion</Button>
                     <a href="#">Olvide mi contraseña</a>
                 </div>
             </div>
