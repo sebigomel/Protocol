b5c3e866c5232612c94b8445fce09a2a35287008
Matilde <71106599+Matilde344@users.noreply.github.com>
2021-10-04 22:30:38 -0300

    el principio de sing up

---
+++ server/models/roleModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const RoleSchema = new Schema({
+    name: String,
+    workspace: {ref: 'Workspace', type: Schema.Types.ObjectId},
+    devices: [ {type: Schema.Types.ObjectId, ref: 'Device'} ]
+});
+
+const Role = mongoose.model('Role', RoleSchema);
+
+module.exports = Role;
