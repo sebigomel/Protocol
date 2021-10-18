449c9efdaaae280607bab74bef256645c54e3796
sebigomel <67985847+sebigomel@users.noreply.github.com>
2021-10-18 20:47:48 -0300

    mas cosas

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,8 +2,8 @@ const User = require("../models/userModel");
 
 module.exports = {
   getOthersInfo: async (req, res) => {
-    username = req.params.username
-    let user = await User.findOne({username: username}).select("-password");
+    username = req.params.username;
+    let user = await User.findOne({ username: username }).select("-password");
     res.status(200).json(user);
   },
   getMyInfo: async (req, res) => {
@@ -11,18 +11,36 @@ module.exports = {
     res.status(200).json(user);
   },
   verifyEmail: async (req, res) => {},
-  update: async (req, res) => {
-    let user = await User.findById(req.user._id);
-    let { cardId, email, password, vaccine, doses} = req.body
-    if (email) {user.verified = false;} 
-    user.cardId = cardId;
-    user.email = email;
-    user.password = password;
-    user.vaccination.vaccine = vaccine;
-    user.vaccination.doses = doses;
 
-    user.save().then(emp => {
+  update: async (req, res) => {
+    const cardId = req.body.cardId;
+    const email = req.body.email;
+    const password = req.body.password;
+    const vaccine = req.body.vaccine;
+    const doses = req.body.doses;
+    let user = await User.findById(req.user._id);
+    console.log(vaccine);
+    switch (true) {
+      case cardId:
+        user.cardId = cardId;
+        break;
+      case email:
+        user.email = email;
+        break;
+      case password:
+        user.password = password;
+        break;
+      case vaccine:
+        user.vaccination.vaccine = vaccine;
+        break;
+      case doses:
+        user.vaccination.doses = doses;
+        break;
+    }
+
+    user.save().then((emp) => {
       res.status(200).send("Your profile has been successfully updated");
-    })
+      console.log(emp);
+    });
   },
 };
--- server/routes/api/user/index.js
+++ server/routes/api/user/index.js
@@ -4,6 +4,6 @@ const UserCtr = require("../../../controllers/user.js");
 module.exports = (app, path) => {
   app.get(path, passport.authenticate("jwt", { session: false }), UserCtr.getMyInfo);
   app.get(path + '/verify/:id/:token', UserCtr.verifyEmail)
-  app.put(path, passport.authenticate("jwt", { session: false }), UserCtr.update)
+  app.patch(path, passport.authenticate("jwt", { session: false }), UserCtr.update)
   app.get(path + '/:username', passport.authenticate("jwt", { session: false }), UserCtr.getOthersInfo)
 };
