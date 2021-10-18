7a7573ad880e4e89e5a81060d98ff89f8513cd07
Matilde <71106599+Matilde344@users.noreply.github.com>
2021-10-18 15:48:52 -0300

    df

--- client/src/Pages/SingUp.jsx
+++ client/src/Pages/SingUp.jsx
@@ -23,7 +23,7 @@ export default function Singup() {
     const classes = useStyles();
     
     return (
-    <form className="singup">
+    <div className="signup">
 
         <img src="/LogoProtocol.png" alt="logo" height="76" width="224" className='icon' />
 
@@ -34,19 +34,28 @@ export default function Singup() {
             <TextField className='login-input' type='email' label='Email' variant="filled" name="email"></TextField>
             <TextField className='login-input' type='password' label='Contraseña' variant="filled" name="password"></TextField>
             <TextField className='login-input' type='password' label='Confirmar Contraseña' variant="filled" name="confirmpassword"></TextField>
-            <TextField className='login-input' type='text' label='Razón' variant="filled" name="razon"></TextField>
+            <TextField className='login-input' type='text' label='Razón/Empresa' variant="filled" name="razon/empresa"></TextField>
+            <TextField className='login-input' type='text' label='User' variant="filled" name="user"></TextField>
+            <TextField className='login-input' type='email' label='Email' variant="filled" name="email"></TextField>
+            <TextField className='login-input' type='password' label='Contraseña' variant="filled" name="password"></TextField>
+            <TextField className='login-input' type='password' label='Confirmar Contraseña' variant="filled" name="confirmpassword"></TextField>
+            <TextField className='login-input' type='text' label='Razón/Empresa' variant="filled" name="razon/empresa"></TextField><TextField className='login-input' type='text' label='User' variant="filled" name="user"></TextField>
+            <TextField className='login-input' type='email' label='Email' variant="filled" name="email"></TextField>
+            <TextField className='login-input' type='password' label='Contraseña' variant="filled" name="password"></TextField>
+            <TextField className='login-input' type='password' label='Confirmar Contraseña' variant="filled" name="confirmpassword"></TextField>
+            <TextField className='login-input' type='text' label='Razón/Empresa' variant="filled" name="razon/empresa"></TextField>
         
             <div className="form-footer">
                 <Button className='custom-btn' variant='contained' color='primary' type="submit">Crear cuenta</Button>
                 <Link to="#" className='forgot-pass' >Ya tengo una cuenta</Link>
             </div>
 
-        </div>
+        </div> 
 
         <div className="footer">
             <img src="/LogoProtocol.png" alt="logo" height="25" />
         </div>
-    </form>
+    </div>
     )
 }
 
